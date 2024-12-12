5f58574993b4c8bdbad1a11b7410c9b21aef6246
Tilek <tilekkoichuev777@gmail.com>
2024-12-12 17:40:16 +0600

    modified Bank Card class. added method printCard, setPassword and string for password and int for numberCard

--- src/model/BankCard.java
+++ src/model/BankCard.java
@@ -1,5 +1,19 @@
 package model;
 
-public class BankCard {
+import java.util.Scanner;
 
+public class BankCard {
+    private Scanner sc=new Scanner(System.in);
+    private int numberCard;
+    private String password;
+
+    public BankCard() {
+
+    }
+    public void printCard(){
+        System.out.println("You passed a check in, now buy something.");
+    }
+    public void setPassword(){
+        System.out.print("Enter your password(any password that is 5 char length): ");
+    }
 }

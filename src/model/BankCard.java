5642ae63afe11c425c8159f462ba42fdbe003a2b
Tilek <tilekkoichuev777@gmail.com>
2024-12-12 17:48:42 +0600

    modified int numberCard to String. Added method setNumberCard and added method in BankCard constructor

--- src/model/BankCard.java
+++ src/model/BankCard.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class BankCard {
     private Scanner sc=new Scanner(System.in);
-    private int numberCard;
+    private String numberCard;
     private String password;
 
     public BankCard() {
+        setNumberCard();
         setPassword();
     }
     public void printCard(){
@@ -23,6 +24,30 @@ public class BankCard {
                 break;
             } else {
                 System.out.println("Password must be 5 characters long. Please try again.");
+            }
+        }
+    }
+    public void setNumberCard() {
+        while (true) {
+            System.out.print("Enter your card ID (16 digits): ");
+            String inputCardId = sc.nextLine();
+            if (inputCardId.length() == 16) {
+                boolean isValid = true;
+                for (int i = 0; i < inputCardId.length(); i++) {
+                    if (!Character.isDigit(inputCardId.charAt(i))) {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (isValid) {
+                    numberCard = inputCardId;
+                    System.out.println("Card ID successfully set.");
+                    break;
+                } else {
+                    System.out.println("Card ID must contain only digits. Please try again.");
+                }
+            } else {
+                System.out.println("Card ID must be 16 digits long. Please try again.");
             }
         }
     }
